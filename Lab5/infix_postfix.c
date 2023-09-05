3bc8758849e2a6bad84b56789214f091b889fc50
RushilJalal <rushiljalal@gmail.com>
2023-09-05 20:19:20 +0530

    infix to postfix code

--- Lab5/infix_postfix.c
+++ Lab5/infix_postfix.c
@@ -1,17 +1,18 @@
+
 #include <stdio.h>
 #define MAX 20
 
-char arr[MAX];//operator stack
+char arr[MAX]; // operator stack
 int top = -1;
 
 int isEmpty()
 {
-    return top==-1;
+    return top == -1;
 }
 
 int isFull()
 {
-    return top = MAX - 1;
+    return top == MAX - 1;
 }
 
 char peek()
@@ -21,11 +22,11 @@ char peek()
 
 char pop()
 {
-    if(isEmpty())
-        {
-            printf("Empty stack! Cannot pop");
-            return -1;
-        }
+    if (isEmpty())
+    {
+        printf("Empty stack! Cannot pop");
+        return -1;
+    }
     char popped = arr[top];
     top--;
     return popped;
@@ -33,7 +34,7 @@ char pop()
 
 void push(char opr)
 {
-    if(isFull())
+    if (isFull())
     {
         printf("Stack is full! Cannot push!");
     }
@@ -46,64 +47,63 @@ void push(char opr)
 
 int checkIfOperand(char ch)
 {
-    return (ch>='a' && ch<='z');
+    return (ch >= 'a' && ch <= 'z');
 }
 
 int precedence(char ch)
 {
-    switch(ch)
+    switch (ch)
     {
-        case '+':
-        case '-':
-            return 1;
+    case '+':
+    case '-':
+        return 1;
 
-        case '*':
-        case '/':
-        case '^':
-            return 2;
+    case '*':
+    case '/':
+    case '^':
+        return 2;
 
-        case '%':
-            return 3;
+    case '%':
+        return 3;
     }
-        return -1;
+    return -1;
 }
 
-int convert(char *statement)//input statement to be converted
-//replace var name statement with expression
+int convert(char *expression) // input expression to be converted
 {
-    char output[MAX];//answer array
-    int i,j;
-    for(i = 0, j = -1; statement[i] != '\0';i++)
+    char output[MAX]; // answer array
+    int i, j;
+    for (i = 0, j = -1; expression[i] != '\0'; i++)
     {
-        if(checkIfOperand(statement[i]))//it is operand(number)
-            output[++j] = statement[i];// so it is directly added to output array
+        if (checkIfOperand(expression[i])) // it is operand(a,b)
+            output[++j] = expression[i];   // so it is directly added to output array
 
-        else if(statement[i] == '(')//encountered open bracket
-            push(statement[i]);//push ( to operator stack
+        else if (expression[i] == '(') // encountered open bracket
+            push(expression[i]);       // push ( to operator stack
 
-        else if(statement[i] == ')')//encountered close bracket
+        else if (expression[i] == ')') // encountered close bracket
         {
-            while(!isEmpty() && peek() != '(')
+            while (!isEmpty() && peek() != '(')
             {
-                output[++j] = pop();//pop all operators until ( is encountered
-            }//or stack is left empty
-            //and add the popped operators to output array
+                output[++j] = pop(); // pop all operators until '(' is encountered
+            }                        // or stack is left empty
+            // and add the popped operators to output array
 
-            if(!isEmpty() && peek() != '(')// non empty stack and peek gives required open bracket
+            if (isEmpty() && peek() != '(') // if stack empty and peek not equal to open bracket,
                 return -1;
 
-            else//doubt
-                pop();//pop the operator
+            else       // doubt
+                pop(); // pop the operator
         }
 
         else
         {
-            while(!isEmpty() && precedence(statement[i]) <= precedence(peek()))
+            while (!isEmpty() && precedence(expression[i]) <= precedence(peek()))
                 output[++j] = pop();
-            push(statement[i]);
+            push(expression[i]);
         }
     }
-    while(!isEmpty())
+    while (!isEmpty())
         output[++j] = pop();
 
     output[++j] = '\0';
@@ -112,7 +112,7 @@ int convert(char *statement)//input statement to be converted
 
 int main()
 {
-    char statement[] = "((x+(y*z))-w)";
-    convert(statement);
+    char expression[] = "((x+(y*z))-w)";
+    convert(expression);
     return 0;
 }
