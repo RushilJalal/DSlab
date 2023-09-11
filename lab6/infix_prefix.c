86213b4cbc5bc8cca73d7c900c85461cd204c310
RushilJalal <rushiljalal@gmail.com>
2023-09-12 00:18:17 +0530

    Fix infix to prefix

--- lab6/infix_prefix.c
+++ lab6/infix_prefix.c
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX 20
-char stk[20];
+
+char stk[MAX];
 int top = -1;
-int isEmpty()
-{
+
+int isEmpty() {
     return top == -1;
 }
-int isFull()
-{
+
+int isFull() {
     return (top == MAX - 1);
 }
-char peek()
-{
+
+char peek() {
     return stk[top];
 }
-char pop()
-{
+
+char pop() {
     if (isEmpty())
         return -1;
     char ch = stk[top];
@@ -25,96 +26,70 @@ char pop()
     return (ch);
 }
 
-void push(char oper)
-{
+void push(char oper) {
     if (isFull())
         printf("Stack Full!!!!");
-    else
-    {
+    else {
         top++;
         stk[top] = oper;
     }
-
 }
-int checkIfOperand(char ch)
-{
+
+int checkIfOperand(char ch) {
     return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
 }
-int precedence(char ch)
-{
-    switch (ch)
-    {
-    case '+':
-    case '-':
-        return 1;
-    case '*':
-    case '/':
-        return 2;
-    case '^':
-    case '%':
-        return 3;
+
+int precedence(char ch) {
+    switch (ch) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+        case '%':
+            return 3;
     }
     return -1;
 }
-char *strrev(char *str)
-{
-    if (!str || !*str)
-        return str;
-    int i = strlen(str) - 1,
-        j = 0;
-    char ch;
-    while (i > j)
-    {
-        ch = str[i];
-        str[i] = str[j];
-        str[j] = ch;
-        i--;
-        j++;
-    }
-    return str;
-}
-int convertInfixToPrefix(char *expression)
-{
+
+void convertInfixToPrefix(char *expression) {
     int i, j;
     char output[20];
     for (i = 0; expression[i] != '\0'; i++)
         ;
     i = i - 1;
-    for (j = -1; i >= 0; i--)
-    {
+    for (j = -1; i >= 0; i--) {
         if (checkIfOperand(expression[i]))
             output[++j] = expression[i];
         else if (expression[i] == ')')
             push(expression[i]);
-        else if (expression[i] == '(')
-        {
+        else if (expression[i] == '(') {
             while (!isEmpty() && peek() != ')')
                 output[++j] = pop();
-            if (!isEmpty() && peek() != ')')
-                return -1;
-            else
+            if (!isEmpty() && peek() != ')') {
+                printf("Mismatched parentheses.\n");
+                return;
+            } else
                 pop();
-        }
-        else
-        {
+        } else {
             while (!isEmpty() && precedence(expression[i]) < precedence(peek()))
                 output[++j] = pop();
             push(expression[i]);
-        } // else
+        }
     }
-    while (!isEmpty())
-    {
+    while (!isEmpty()) {
         output[++j] = pop();
-        output[++j] = '\0';
-        strrev(output);
-        printf("%s", output);
     }
+    output[++j] = '\0';
+    // Reverse the output string
+    strrev(output);
+    printf("%s\n", output);
 }
-int main()
-{
+
+int main() {
     char expression[] = "a+b-c";
     convertInfixToPrefix(expression);
     return 0;
 }
-
-// no output, check
