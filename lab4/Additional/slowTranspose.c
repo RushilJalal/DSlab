44a1579bceb3b34cccef727d5b03efb927a153f8
RushilJalal <rushiljalal@gmail.com>
2023-09-13 13:16:53 +0530

    remove comments

--- lab4/Additional/slowTranspose.c
+++ lab4/Additional/slowTranspose.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Define a structure to represent a sparse matrix element
 typedef struct
 {
     int row;
@@ -8,7 +7,6 @@ typedef struct
     int value;
 } MatrixElement;
 
-// Function to perform slow transpose of a sparse matrix
 void slowTranspose(MatrixElement input[], MatrixElement output[], int numRows, int numCols)
 {
     int outputIndex = 0;
@@ -30,19 +28,15 @@ void slowTranspose(MatrixElement input[], MatrixElement output[], int numRows, i
 
 int main()
 {
-    // Define the size of the matrix
     int numRows, numCols;
 
     printf("Enter the number of rows and columns in the matrix: ");
     scanf("%d %d", &numRows, &numCols);
 
-    // Calculate the maximum possible number of non-zero elements
     int maxNonZero = numRows * numCols;
 
-    // Create an array to store the input matrix
     MatrixElement inputMatrix[maxNonZero];
 
-    // Take input for the matrix
     printf("Enter the elements of the matrix (row, col, value):\n");
     int numNonZero;
     scanf("%d", &numNonZero);
@@ -53,13 +47,10 @@ int main()
         scanf("%d %d %d", &inputMatrix[i].row, &inputMatrix[i].col, &inputMatrix[i].value);
     }
 
-    // Create an array to store the transposed matrix
     MatrixElement outputMatrix[maxNonZero];
 
-    // Perform the slow transpose operation
     slowTranspose(inputMatrix, outputMatrix, numRows, numCols);
 
-    // Print the transposed matrix
     printf("Transposed Matrix:\n");
     for (int i = 0; i < numNonZero; i++)
     {
