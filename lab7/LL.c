a040b00c04af187796b69abb1466493c8713a774
RushilJalal <rushiljalal@gmail.com>
2023-09-21 13:27:06 +0530

    delete function

--- lab7/LL.c
+++ lab7/LL.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
+typedef struct Node
 {
     int data;
-    struct node *link;
+    Node *link;
 } Node;
 
 Node *start = NULL;
@@ -50,7 +50,7 @@ void traverse()
         printf("Empty list");
     else
     {
-        printf("Displaying list: /n");
+        printf("Displaying list: \n");
         current = start;
         while (current != NULL)
         {
@@ -126,10 +126,36 @@ void insertAfter()
         printf("Data not found\n");
 }
 
+void delete()
+{
+    int del, found = 0;
+    Node *previous;
+    Node *current;
+    current = malloc(sizeof(Node));
+    previous = NULL;
+    printf("Enter element to be deleted: ");
+    scanf("%d", &del);
+
+    current = start;
+    while (current != NULL)
+    {
+        if (current->data == del)
+        {
+            previous->link = current->link;
+            printf("Element %d deleted\n", del);
+            found = 1;
+            return;
+        }
+        previous = current;
+        current = current->link;
+    }
+    if (found == 0)
+        printf("Element not found");
+}
+
 int main()
 {
     createList();
-    insertBefore();
-    insertAfter();
+    delete ();
     traverse();
 }
